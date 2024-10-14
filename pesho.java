f894f5ee570a2d49a2891ed3f315ac81e250db2b
Daniel-Kolev777 <danielkolev2006@abv.bg>
2024-10-14 13:33:09 +0300

    newBranch

--- pesho.java
+++ pesho.java
@@ -1,9 +1,6 @@
 public class pesho {
     public static void main(String[] args) {
-        System.out.println("Dankata!!!");
-
-        System.out.println("Kiro!!!");
-        System.out.println("Ivan!");
+        System.out.println("Hello world!!");
 
     }
 
